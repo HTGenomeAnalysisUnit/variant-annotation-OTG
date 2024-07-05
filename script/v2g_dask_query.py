2e32254c5b94e61c5bb4d577c5019abbcff7084a
bruno-ariano <bruno.ariano.87@gmail.com>
2024-07-05 17:24:04 +0200

    change formatting

--- script/v2g_dask_query.py
+++ script/v2g_dask_query.py
@@ -2,6 +2,7 @@ import dask.dataframe as dd
 from dask.distributed import LocalCluster
 import numpy as np
 import argparse
+import pandas as pd
 
 parser = argparse.ArgumentParser(
                     prog='OTG query v2g',
@@ -59,11 +60,11 @@ def main():
 	ddf = dd.read_parquet("/ssu/gassu/reference_data/OpenTargets/22.10/v2g_scored",dtype = dtypes)
 	
 	columns = list(ddf.columns)
-	columns_select = columns[0:8] + columns[12:14] + [columns[22]]
+	columns_select = columns[0:8] + columns[10:17] + [columns[20]] + [columns[22]]
 	ddf_select = ddf[columns_select]
 	
 	#Set the SNP variable made of chr_pos_ref_alt
-	ddf["SNP_id"] = ddf["chr_id"] + "_" + ddf["position"].astype(str)  + "_" + ddf["ref_allele"]  + "_"  + ddf["alt_allele"]
+	ddf_select["SNP_id"] = ddf_select["chr_id"] + "_" + ddf_select["position"].astype(str)  + "_" + ddf_select["ref_allele"]  + "_"  + ddf_select["alt_allele"]
 	
 	#Read variants to query and convert to Dask series
 	variant_query = dd.read_csv(args.input)
@@ -73,33 +74,50 @@ def main():
 	filtered_ddf = ddf_select[ddf_select['SNP_id'].isin(snp_values)].compute()
       
 	#Aggregate data produced
-	def aggregate_list(values, type_filter, filtered_ddf):
-		filtered_values = list(values[filtered_ddf['type_id'] == type_filter])
-		return filtered_values if filtered_values else np.nan
+	def aggregate_list_qtl(group, type_id):
+		filtered_group = group[group['type_id'] == type_id]
+		qtl_scores = filtered_group['qtl_score'].tolist() if not filtered_group.empty else np.nan
+		features = filtered_group['feature'].tolist() if not filtered_group.empty else np.nan
+		return qtl_scores,features
 
-	grouped_filtered = filtered_ddf.groupby(['SNP_id', 'gene_id']).agg(
-    	chr_id=('chr_id', 'first'),
-    	position=('position', 'first'),
-    	ref_allele=('ref_allele', 'first'),
-    	alt_allele=('alt_allele', 'first'),
-    	distance_score=('distance_score', 'first'),
-    	fpred_max=('fpred_max_score', 'max'),
-    	fpred_label=('fpred_max_label', 'first'),
-    	phic_scores=('qtl_score', lambda x: aggregate_list(x, 'phic', filtered_ddf)),
-    	phic_features=('feature', lambda x: aggregate_list(x, 'phic', filtered_ddf)),
-    	eqtl_scores=('qtl_score', lambda x: aggregate_list(x, 'eqtl', filtered_ddf)),
-    	eqtl_features=('feature', lambda x: aggregate_list(x, 'eqtl', filtered_ddf)),
-    	pqtl_scores=('qtl_score', lambda x: aggregate_list(x, 'pqtl', filtered_ddf)),
-    	pqtl_features=('feature', lambda x: aggregate_list(x, 'pqtl', filtered_ddf)),
-    	sqtl_scores=('qtl_score', lambda x: aggregate_list(x, 'sqtl', filtered_ddf)),
-    	sqtl_features=('feature', lambda x: aggregate_list(x, 'sqtl', filtered_ddf)),
-    	overall_scores=('overall_score', 'first'),
-	).reset_index()
-	
-	grouped_filtered.to_csv(args.out, sep = "\t")
+	def aggregate_list_intervals(group, type_id):
+		filtered_group = group[group['type_id'] == type_id]
+		interval_scores = filtered_group['interval_score'].tolist() if not filtered_group.empty else np.nan
+		#source = filtered_group['source_id'].tolist() if not filtered_group.empty else np.nan
+		return (interval_scores)
 
+	# Custom aggregation function
+	def custom_agg(group):
+		pchic_scores = aggregate_list_intervals(group, 'pchic')
+		fantom5_scores = aggregate_list_intervals(group, 'fantom5')
+		dhs_scores = aggregate_list_intervals(group, 'dhscor')
+		eqtl_scores, eqtl_features = aggregate_list_qtl(group, 'eqtl')
+		pqtl_scores, pqtl_features = aggregate_list_qtl(group, 'pqtl')
+		sqtl_scores, sqtl_features = aggregate_list_qtl(group, 'sqtl')
+
+		return pd.Series({
+		'chr_id': group['chr_id'].iloc[0],
+		'position': group['position'].iloc[0],
+		'ref_allele': group['ref_allele'].iloc[0],
+		'alt_allele': group['alt_allele'].iloc[0],
+		'distance_score': group['distance_score'].iloc[0],
+		'fpred_max': group['fpred_max_score'].max(),
+		'fpred_label': group['fpred_max_label'].iloc[0],
+		'dhs_scores' :	np.nanmax(dhs_scores) if isinstance(dhs_scores, list) else np.nan,
+		'max_pchic_score': np.nanmax(pchic_scores) if isinstance(pchic_scores, list) else np.nan,
+		'max_fantom5_score': np.nanmax(fantom5_scores) if isinstance(fantom5_scores, list) else np.nan,
+		'eqtl_scores': eqtl_scores,
+		'eqtl_features': eqtl_features,
+		'pqtl_scores': pqtl_scores,
+		'pqtl_features': pqtl_features,
+		'sqtl_scores': sqtl_scores,
+		'sqtl_features': sqtl_features,
+		'overall_scores': group['overall_score'].iloc[0]
+		})
+
+	# Group by SNP_id and gene_id and apply the custom aggregation function
+	grouped_filtered = filtered_ddf.groupby(['SNP_id', 'gene_id']).apply(custom_agg).reset_index()
+	grouped_filtered.to_csv(args.out, sep = "\t", index = False)
 
 if __name__ == '__main__':
     main()
-
-
